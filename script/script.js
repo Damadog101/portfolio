5d5b3e08ee2146e2a7d20251e33157c1958004b4
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-04-24 12:16:16 -0700

    Update script.js
    possibly fixed home hover bug

--- script/script.js
+++ script/script.js
@@ -2,11 +2,16 @@ String.fromCharCode(97+~~(Math.random()*26))
 
 let names = document.getElementById("name")
 const letters = "abcdefghijklmnopqrstuvwxyz"
+ 
+let interval = null
 
 names.addEventListener("mouseover", () => {
 
     let original = names.innerText
     let iterations = 0
+    
+
+    clearInterval(interval)
 
     const interval = setInterval(() => {
         names.innerText =  names.innerText.split("").map((letter, index) => {
