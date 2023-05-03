be93a518750ee6bc7c68f7aec11e5ea0fbb4895d
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-03 10:42:03 -0700

    hobbies hover being weird

--- script/changeText.js
+++ script/changeText.js
@@ -1,7 +1,8 @@
 let awardOne = document.getElementById("awardOne");
 let awardTwo = document.getElementById("awardTwo");
 let awardThree = document.getElementById("awardThree");
-
+let hobbyOne = document.getElementById("hobbyOne")
+let hobbyTwo = document.getElementById("hobbyTwo")
 
 awardOne.addEventListener("mouseover", () => {
     awardOne.innerHTML = "Throughout my two years of middle school, I did my best to get the best grades I could in all my classes. In the end, my effort was rewarded, as I earned the Principal's Medallion medal for maintaining straight A’s throughout all of middle school."
@@ -22,4 +23,18 @@ awardThree.addEventListener("mouseover", () => {
 })
 awardThree.addEventListener("mouseleave", () => {
     awardThree.innerHTML = "Rubik's Cube Award"
+})
+
+hobbyOne.addEventListener("mouseover", () => {
+    hobbyOne.innerHTML = "Skill toys are one of my favorite things, and I am almost never without one. My main 3 skills of choice are speedsolving Rubik's Cubes, using kendamas, and juggling. Kendama is my oldest hobby, and has shown the most improvement. We all start by just barely catching it on one cup, but I have improved my skill to be able to juggle it around, balance it in unique spots, and finish by catching it on the spike. Rubik's Cubes are more recent, but most familiar to the average person. I average 12 seconds a solve, with a best of 7.12. In addition to this, I can solve them blindfolded, and have grown my collection to about 100 Rubik's-like puzzles. Lastly is juggling, which I took on to combine my other skills together. As stated, I can juggle the parts of a kendama to perform more creative tricks. However, I have taken it a step further, being able to juggle and solve Rubiks Cubes at the same time."
+})
+hobbyOne.addEventListener("mouseleave", () => {
+    hobbyOne.innerHTML = "Skill Toys"
+})
+
+hobbyTwo.addEventListener("mouseover", () => {
+    hobbyTwo.innerHTML = "Like most people my age, I greatly enjoy video games. I am overall a Nintendo supporter, with my favorite series being the Mario and Metroid series, and my favorite game being Super Mario Galaxy 2. However, I have more recently been trying out some other kinds of games, and have come to greatly enjoy Subnautica, Just Dance, and Minecraft."
+})
+hobbyTwo.addEventListener("mouseleave", () => {
+    hobbyTwo.innerHTML = "Gaming"
 })
