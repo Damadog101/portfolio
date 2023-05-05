2c047928d2cbfec4fae3b753707d34c7af71ffcf
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-05 08:14:18 -0700

    somewhat fixed the home page cycle thingy only on hover

--- script/textCycle.js
+++ script/textCycle.js
@@ -3,16 +3,34 @@ String.fromCharCode(97 + ~~(Math.random() * 26));
 let names = document.getElementById("name");
 const letters = "abcdefghijklmnopqrstuvwxyz";
 
+const textCycle = {
+	1: "Develop Websites",
+	2: "Edit Videos",
+	3: "Write Essays",
+	4: "Program With JavaScript",
+	5: "Wireframe with Figma",
+	6: "Markup with HTML and CSS"
+}
+
+
+
+
+
+
+
+
 names.addEventListener("mouseover", () => {
 	let original = names.innerText;
 	let iterations = 0;
 
 	const interval = setInterval(() => {
+		let num = Math.ceil(Math.random() * 7);
+
 		names.innerText = names.innerText
 			.split("")
 			.map((letter, index) => {
 				if (index < iterations) {
-					return original[index];
+					return textCycle[num][index];
 				}
 				return letters[Math.floor(Math.random() * 26)];
 			})
