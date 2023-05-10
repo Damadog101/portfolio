514517a2a1603530c418081b0cc77221c72fb118
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-10 10:41:19 -0700

    the idea for perfect branches is done just need to take a long time to implement it

--- script/tree.js
+++ script/tree.js
@@ -1,15 +1,47 @@
 let treeBase = document.getElementById("treeBase");
-let leftProjectBase = document.getElementById("branchLeft");
-let rightProjectBase = document.getElementById("branchRight");
 
+
+let leftProjectBase = document.getElementById("project1");
+let rightProjectBase = document.getElementById("project2");
+
+
+let treeWrappers = document.querySelectorAll(".treeWrap")
+let emptyDivs = document.querySelectorAll(".empty");
+let trees = document.querySelectorAll(".tree");
 let leftProjects = document.querySelectorAll(".projectOdd");
 let rightProjects = document.querySelectorAll(".projectEven");
 
 
 window.addEventListener("resize", () => {
-    let leftWidth = treeBase.style.left - leftProjectBase.clientWidth;
+
+
+    let projectsHeight = rightProjectBase.offsetHeight;
+    let projectsWidth = rightProjectBase.offsetWidth;
+
+
+    // // [...trees].forEach((item) => {
+	// // 	item.style.height = `${projectsHeight + 100}px`;
+    // //     item.style.width = `${projectsWidth + 100}px`;
+
+	// // });
+    // [...treeWrappers].forEach((item) => {
+	// 	item.style.height = `${projectsHeight + 100}px`;
+    //     // item.style.width = `${projectsWidth + 100}px`;
+
+	// });
+    // [...emptyDivs].forEach((item) => {
+	// 	item.style.height = `${projectsHeight + 100}px`;
+    //     // item.style.width = `${projectsWidth + 100}px`;
+
+	// });
+
+
+    console.log(leftProjectBase.style.left)
+    let leftWidth = treeBase.style.left - rightProjectBase.clientWidth;
     let rightWidth = treeBase.clientWidth - leftProjectBase.style.left;
 
+    console.log("leftwidth: " + leftWidth)
+    console.log("rightwidth: " + rightWidth)
     
     
 
