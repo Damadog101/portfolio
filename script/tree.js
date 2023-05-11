5f1cbd53451fb4fd62a277ec05cb8495da0098c8
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-11 08:16:47 -0700

    branches work for all sizes now

--- script/tree.js
+++ script/tree.js
@@ -1,48 +1,48 @@
-let treeBase = document.getElementById("treeBase");
+// let treeBase = document.getElementById("treeBase");
 
 
-let leftProjectBase = document.getElementById("project1");
-let rightProjectBase = document.getElementById("project2");
+// let leftProjectBase = document.getElementById("project1");
+// let rightProjectBase = document.getElementById("project2");
 
 
-let treeWrappers = document.querySelectorAll(".treeWrap")
-let emptyDivs = document.querySelectorAll(".empty");
-let trees = document.querySelectorAll(".tree");
-let leftProjects = document.querySelectorAll(".projectOdd");
-let rightProjects = document.querySelectorAll(".projectEven");
+// let treeWrappers = document.querySelectorAll(".treeWrap")
+// let emptyDivs = document.querySelectorAll(".empty");
+// let trees = document.querySelectorAll(".tree");
+// let leftProjects = document.querySelectorAll(".projectOdd");
+// let rightProjects = document.querySelectorAll(".projectEven");
 
 
-window.addEventListener("resize", () => {
+// window.addEventListener("resize", () => {
 
 
-    let projectsHeight = rightProjectBase.offsetHeight;
-    let projectsWidth = rightProjectBase.offsetWidth;
+//     let projectsHeight = rightProjectBase.offsetHeight;
+//     let projectsWidth = rightProjectBase.offsetWidth;
 
 
-    // // [...trees].forEach((item) => {
-	// // 	item.style.height = `${projectsHeight + 100}px`;
-    // //     item.style.width = `${projectsWidth + 100}px`;
+//     // // [...trees].forEach((item) => {
+// 	// // 	item.style.height = `${projectsHeight + 100}px`;
+//     // //     item.style.width = `${projectsWidth + 100}px`;
 
-	// // });
-    // [...treeWrappers].forEach((item) => {
-	// 	item.style.height = `${projectsHeight + 100}px`;
-    //     // item.style.width = `${projectsWidth + 100}px`;
+// 	// // });
+//     // [...treeWrappers].forEach((item) => {
+// 	// 	item.style.height = `${projectsHeight + 100}px`;
+//     //     // item.style.width = `${projectsWidth + 100}px`;
 
-	// });
-    // [...emptyDivs].forEach((item) => {
-	// 	item.style.height = `${projectsHeight + 100}px`;
-    //     // item.style.width = `${projectsWidth + 100}px`;
+// 	// });
+//     // [...emptyDivs].forEach((item) => {
+// 	// 	item.style.height = `${projectsHeight + 100}px`;
+//     //     // item.style.width = `${projectsWidth + 100}px`;
 
-	// });
+// 	// });
 
 
-    console.log(leftProjectBase.style.left)
-    let leftWidth = treeBase.style.left - rightProjectBase.clientWidth;
-    let rightWidth = treeBase.clientWidth - leftProjectBase.style.left;
+//     console.log(leftProjectBase.style.left)
+//     let leftWidth = treeBase.style.left - rightProjectBase.clientWidth;
+//     let rightWidth = treeBase.clientWidth - leftProjectBase.style.left;
 
-    console.log("leftwidth: " + leftWidth)
-    console.log("rightwidth: " + rightWidth)
+//     console.log("leftwidth: " + leftWidth)
+//     console.log("rightwidth: " + rightWidth)
     
     
 
-})
+// })
