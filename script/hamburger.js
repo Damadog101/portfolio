30f436acb14a799f8a8d365673fd00b242f43181
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-13 19:23:32 -0700

    working with mobile

---
+++ script/hamburger.js
@@ -0,0 +1,28 @@
+let mobile = document.getElementById("mobileLayout");
+let normal = document.getElementById("normalLayout");
+let hamburger = document.getElementById("hamburger");
+let links = document.getElementById("mobileLinks");
+
+function showHamburger() {
+	let width = window.innerWidth;
+	if (width >= 500) {
+		hamburger.classList.add("noDisplay");
+		mobile.classList.add("noDisplay");
+
+		normal.classList.remove("noDisplay");
+
+		return;
+	} else {
+		hamburger.classList.remove("noDisplay");
+		mobile.classList.remove("noDisplay");
+
+		normal.classList.add("noDisplay");
+	}
+}
+
+window.addEventListener("resize", () => {
+	showHamburger();
+});
+window.addEventListener("load", () => {
+	showHamburger();
+});
